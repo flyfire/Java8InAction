0364ca43ca49b3f8338f3d93fea518cc69a0aff6
Solarex <rh.hou.work@gmail.com>
2017-09-25 15:27:44 +0800

    hello

---
+++ src/main/java/com/solarexsoft/java8inaction/Hello.java
@@ -0,0 +1,10 @@
+package com.solarexsoft.java8inaction;
+
+/**
+ * Created by houruhou on 25/09/2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
